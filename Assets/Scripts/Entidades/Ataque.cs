e76ad13dba2ea60f5bd1fc55d9657761adfd09b6
PabloGHub <149364828+PabloGHub@users.noreply.github.com>
2025-04-13 15:21:22 +0200

    Ataque casi terminao

--- Assets/Scripts/Entidades/Ataque.cs
+++ Assets/Scripts/Entidades/Ataque.cs
@@ -5,9 +5,11 @@ public class Ataque : MonoBehaviour
     // ***********************( Declaraciones )*********************** //x
     // ----( Atribustos )---- //
     [Header("*-- Atribustos --*")]
-    [SerializeField] private float danno = 1;
-    [SerializeField] private float alcance = 1;
-    [SerializeField] private float tiempoRecarga = 1;
+    [SerializeField] private float danno = 1f;
+    [SerializeField] private float alcance = 1f;
+    [SerializeField] private float tiempoRecarga = 1f;
+    [SerializeField] private float fuerzaEmpuje = 5f;
+    [SerializeField] private bool explosion = false;
 
     private float v_tiempoDeRecargaAtual_f;
 
@@ -17,18 +19,21 @@ public class Ataque : MonoBehaviour
     [HideInInspector] public Vector3? v_inicio_V3 = null;
     //[HideInInspector] public float v_alcance_f;
 
+    // ----( Componentes )---- //
+    private Salud v_salud_s;
+
     // ***********************( Metodos UNITY )*********************** //
     private void Start()
     {
         v_tiempoDeRecargaAtual_f = 0f;
+
+        v_salud_s = GetComponent<Salud>();
     }
 
     private void Update()
     {
         if (v_tiempoDeRecargaAtual_f > 0)
-        {
             v_tiempoDeRecargaAtual_f -= Time.deltaTime;
-        }
     }
 
     // ***********************( Metodos NUESTROS )*********************** //
@@ -49,18 +54,24 @@ public class Ataque : MonoBehaviour
         Vector2 _direccion = new Vector2(v_direcion_f.Value, 0);
         Vector3 _inicio = (v_inicio_V3 != null) ? v_inicio_V3.Value : transform.position;
 
-        RaycastHit2D v_golpe = Physics2D.Raycast(_inicio, _direccion, alcance, v_capaAtacado_LM.Value);
-        if (v_golpe)
+        RaycastHit2D _golpe = Physics2D.Raycast(_inicio, _direccion, alcance, v_capaAtacado_LM.Value);
+        if (_golpe)
         {
             if (v_tiempoDeRecargaAtual_f <= 0)
             {
-                Debug.Log("Golpeado: " + v_golpe.collider.name);
-                // Aqui se puede aplicar el daño al objeto golpeado
-                // v_golpe.collider.GetComponent<Salud>().RecibirDano(danno);
+                Debug.Log("Golpeado: " + _golpe.collider.name);
+
+                Salud _salud = _golpe.collider.GetComponent<Salud>();
+                if (_salud != null)
+                {
+                    float? _ref = _salud.RecibirDano(danno, fuerzaEmpuje);
+
+                    if (_ref != null && v_salud_s != null)
+                        v_salud_s.RecibirDano((float)_ref);
+                }
             }
         }
 
         v_tiempoDeRecargaAtual_f = tiempoRecarga;
-        
     }
 }
