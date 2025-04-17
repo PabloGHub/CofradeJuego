36b9ef4be59f4c7a3c7ad16cf6de2715a8fc0d6a
PabloGHub <149364828+PabloGHub@users.noreply.github.com>
2025-04-18 00:19:19 +0200

    ProyectilCera

---
+++ Assets/Scripts/Entidades/Proyectil/Proyectil.cs
@@ -0,0 +1,91 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+// NOTA: Copilot el proyecto es 2D.
+public class Proyectil : MonoBehaviour
+{
+    // ***********************( Declaraciones )*********************** //
+    [Header("*-- Atributos --*")]
+    [SerializeField] private float _velocidad_f = 10f;
+    [SerializeField] private float _maxTiempoVuelo_f = 10f;
+
+    [Header("*-- Prefab de Explosion --*")]
+    [SerializeField] private GameObject _prefabExplosion_go;
+
+    [Header("*-- Lista de capas Colisionar --*")]
+    [SerializeField] private List<LayerMask> _listaCapas;
+
+    private float _tiempoRestante_f = 0f;
+    private RaycastHit2D _anteriorJit;
+    private LayerMask _mascara;
+
+    // ***********************( Metodos Unity )*********************** //
+    private void Awake()
+    {
+        _tiempoRestante_f = _maxTiempoVuelo_f;
+        _mascara = F_CombinarCapas_LM(_listaCapas);
+    }
+
+    private void Update()
+    {
+        _tiempoRestante_f -= Time.deltaTime;
+        if (_tiempoRestante_f <= 0f)
+            colisionar();
+
+        transform.Translate(Vector2.right * _velocidad_f * Time.deltaTime);
+
+        RaycastHit2D _hit = Physics2D.Raycast
+        (
+            transform.position,
+            transform.up,
+            _velocidad_f * Time.deltaTime,
+            _mascara
+        );
+        if (_hit)
+        {
+            _anteriorJit = _hit;
+            if (_hit.collider.gameObject.layer == LayerMask.NameToLayer("Proyectil"))
+                return;
+
+            colisionar();
+        }
+    }
+
+    // ***********************( Metodos Nuestros )*********************** //
+    private void colisionar()
+    {
+        GameObject _explosion = Instantiate(_prefabExplosion_go, transform.position, Quaternion.identity);
+        _explosion.transform.localScale = new Vector3(1f, 1f, 1f);
+        Destroy(gameObject);
+    }
+
+    public LayerMask F_CombinarCapas_LM(List<LayerMask> _capas)
+    {
+        int _combinadas = 0;
+        foreach (LayerMask _capa in _capas)
+        {
+            _combinadas |= _capa.value;
+        }
+        return _combinadas;
+    }
+
+    // ***********************( Gizmos )*********************** //
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _maxTiempoVuelo_f);
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.up * _velocidad_f);
+
+        {
+        #if UNITY_EDITOR
+            Handles.Label(transform.position + Vector3.up * 0.5f,
+                            $"PROYECTIL : tiempoRestante -> {_tiempoRestante_f} | hitAnterior -> {_anteriorJit}");
+        #endif
+        }
+
+    }
+}
